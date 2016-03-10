5bc49dcfe79aa2794caca0f263492cef4e43ef63
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-10 17:57:35 -0600

    half way working code

--- 02-hide-many-elements/global.js
+++ 02-hide-many-elements/global.js
@@ -1,18 +1,27 @@
 window.onload = function() {
 
+    var my_function = function(){
+    texts = document.getElementsByClassName("hide_me");
+    var i = 0;
+    if (texts.length > i) {
+      texts.style.display = "none";
+    }
+  }
+
   // Here is some pseudo-code to help you get started:
 
   // 1. Get the DOM element which will be clicked.
   var hide_button = document.getElementById("button");
 
-  var text = document.getElementByClass("hide_me")
-
   // 2. Add a listener for the 'click' event onto that element.
-  hide_button.addEventListener("click",text.hide);
+  hide_button.addEventListener("click",my_function);
+
+  
     
     // 3. The block for the listener should get the DOM
     //    element containing the text to hide.
     
+  
 
       
     // 4. Modify that DOM element's style to change it's 'display'
@@ -24,13 +33,3 @@ window.onload = function() {
 
 };
 
-
-ruby method
-
-def hide_text
-  texts = Element.where(Class = "hide_me")
-  texts.each do |text|
-    text.hide
-  end
-
-end
