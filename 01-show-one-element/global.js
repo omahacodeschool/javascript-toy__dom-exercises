e62370f1c5bb08a1bce80d77529ed3fb9e6e5673
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-10 12:11:57 -0600

    01 cleanup and done

--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -1,9 +1,4 @@
 window.onload = function() {
-
-
-  // Here is some pseudo-code to help you get started:
-
-  // 1. Get the DOM element which will be clicked.
   var click_it = document.getElementById("more_text_link");
   click_it.addEventListener("click", function(){
     var text_to_show = document.getElementById("more_text_content");
