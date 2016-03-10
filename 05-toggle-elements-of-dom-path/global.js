eed1aace57a82928aa60a9c51410fa0db6286cf5
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-10 17:56:40 -0600

    05 code and question done

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here.
+
+
+window.onload = function() {
+
+  var button = document.getElementById('button');
+
+  button.addEventListener("click", function changeButtonz() {
+
+    var divs_to_toggle = document.querySelectorAll('ul.second_five li');
+
+//added a little extra functionality to change the value (displayed text) of the button depending on the state of my divs.
+    for (var i = 0; i < divs_to_toggle.length; i++) {
+      if (divs_to_toggle[i].style.display === "list-item") {
+        divs_to_toggle[i].style.display = "none";
+        button.value = "Show"
+      }
+      else {
+        divs_to_toggle[i].style.display = "list-item";
+        button.value = "Hide"
+      }
+    }
+  });
+
+};
