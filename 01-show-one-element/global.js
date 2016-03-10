0316b3420c782cc50fe09903fbb57109876030d1
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-10 11:35:04 -0600

    did first exercise in class

--- 01-show-one-element/global.js
+++ 01-show-one-element/global.js
@@ -1,17 +1,13 @@
 window.onload = function() {
 
-  // Here is some pseudo-code to help you get started:
+  var click_it = document.getElementById("more_text_link");
 
-  // 1. Get the DOM element which will be clicked.
+  click_it.addEventListener("click", function(){
+    var textToShow = document.getElementById("more_text_content");
 
-  // 2. Add a listener for the 'click' event onto that element.
+    textToShow.style.display = "inline";
 
-  // 3. The block for the listener should get the DOM
-  //    element containing the text to reveal.
-
-  // 4. Modify that DOM element's style to change it's 'display'
-  //    from a hidden value to a shown value.
-
-  // 5. Also modify the DOM to hide the "More info..." link.
+    click_it.style.display = "none";
+  });
   
 };
