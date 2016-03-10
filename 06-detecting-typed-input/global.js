b9b590e424a268c4b4f0caae0fde2b299d8ae77a
Kyle Sandage <ksandage08@gmail.com>
2016-03-10 14:48:39 -0600

    question

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,11 @@
-// Your JavaScript goes here.
+window.onload = function() {
+  var current_text = document.getElementById("full_name")
+
+  var mirror = document.getElementById("greeting")
+
+  var returnedText = function(){
+    for (var i = 0; i < current_text.length; i++) {
+    mirror.innerHTML = mirrored_text[i];
+    };
+  };
+};
