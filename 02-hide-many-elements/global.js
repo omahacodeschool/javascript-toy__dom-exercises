b5bea35418050bc26ad9bbbc1559db3f4bf4bbe9
Patrick Clark <patclark1958@gmail.com>
2016-03-10 13:42:32 -0600

    At last 02 is done. The console is my friend. Two typos (getElementsbyID had to become getElementsById) kill 20 minutes. Also a little baffled about when and where to open and close a curly bracket.

--- 02-hide-many-elements/global.js
+++ 02-hide-many-elements/global.js
@@ -2,34 +2,22 @@
 window.onload = function() {
 
 //if use of the button is the triggering event, I need an event listener for the button. 
+  var button = document.getElementById("button");
 
-  button.addEventListener("click", function()) {
+  button.addEventListener("click", function() {
 
-//things to hide findByClass("hide_me") I see now in js that is getElementsByClassName.
+//things to hide findByClass("hide_me") no that's more ruby I see now in js that is getElementsByClassName.
 
-    var hideMe = document.getElementsByClassName("hide_me")
+    var hideMe = document.getElementsByClassName("hide_me");
 
 //getElementsByClassName makes an array? 
 //so everything in the array hideMe I want to hide. We don't do each-do?
 //Courtesy of w3schools description of for loops:
-    for (count = 0; count < hideMe.length; count++) {
-      hideMe[count].style.display = none
+    for (index = 0; index < hideMe.length; index+=1) {
+      hideMe[index].style.display = "none";
 
     }//end of for loop curly bracket
-  }//end of add event listener curly bracket
-}//end of window.onload curly bracket
+  });//end of add event listener curly bracket
+};//end of window.onload curly bracket
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-}
