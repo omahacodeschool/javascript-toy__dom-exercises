2a036c6fc98146800eb20e1a696071e9edd49e09
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-10 17:12:51 -0600

    working on 7

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,8 @@
-// Your JavaScript goes here.
+window.onload = function() {  
+  var tab_over_first = document.getElementById("first");
+  tab_over_first.addEventListener("keyup", function() {
+    if (tab_over_first.length === 3){
+      //tab over to the next box however you do that
+    }
+  })//ends function in event listener    
+};//ends window.onload// Your JavaScript goes here.
