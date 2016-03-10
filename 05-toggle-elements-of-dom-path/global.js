59e223fc000d0706a8ef8e2720d20f27cf586c4a
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-10 13:23:33 -0600

    Exercise 5 finished

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,21 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+  var button = document.getElementById('toggle_button');
+
+  button.addEventListener("click", function() {
+
+    var text_to_toggle = document.querySelectorAll('.second_five li');
+
+    if (text_to_toggle[0].style.display == "none") {
+      for (var i = 0; i < text_to_toggle.length; i++) {
+        text_to_toggle[i].style.display = "block";
+      }
+    } else {
+      for (var i = 0; i < text_to_toggle.length; i++) {
+        text_to_toggle[i].style.display = "none";
+      }
+    }
+
+  });
+
+};
