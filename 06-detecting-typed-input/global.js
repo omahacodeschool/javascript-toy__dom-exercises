ac9f2fd49ed6b2f5424eaf06a6ac006d45ed8d6b
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-10 16:48:37 -0600

    almost there 6

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,10 @@
-// Your JavaScript goes here.
+window.onload = function() {  
+  var mirror_text = document.getElementById("full_name");
+  mirror_text.addEventListener("keyup", function() {
+    var letter = document.getElementById("full_name").value;
+    var mirror = document.getElementById("greeting");
+    mirror = letter
+    console.log(letter)
+    console.log(mirror)
+  })//ends function in event listener    
+};//ends window.onload// Your JavaScript goes here.
