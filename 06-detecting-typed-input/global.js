59ee0c3df2b203553530ffebd30e7b7a58d84fb9
Ryan Newell <youcatastrophe@gmail.com>
2016-03-10 15:46:08 -0600

    Working Code for 06 Detecting Typed Input

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+  var nameInput= document.getElementById('full_name');
+
+  nameInput.addEventListener("keyup", function() {
+
+    var greeting = document.getElementById("greeting");
+
+    greeting.innerHTML = ("Hi, " + nameInput.value + "!!!");
+
+  });
+
+};
