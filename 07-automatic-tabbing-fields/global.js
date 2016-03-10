e5801c384f1ac76f2a98ce36fddd5bfd0a098a23
Beth Haubert <bethany.haubert@gmail.com>
2016-03-10 13:49:20 -0600

    07 done

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+	var first_input = document.getElementById("first");
+	var second_input = document.getElementById("second");
+	var third_input = document.getElementById("third");
+
+	first_input.addEventListener("keyup", function() {
+		typed_characters = first_input.value.length
+		max_length = parseInt(first_input.getAttribute("maxlength"));
+
+		if (typed_characters === max_length) {
+			second_input.focus();
+		}
+	});
+	second_input.addEventListener("keyup", function() {
+		typed_characters = second_input.value.length
+		max_length = parseInt(second_input.getAttribute("maxlength"));
+
+		if (typed_characters === max_length) {
+			third_input.focus();
+		}
+	});
+
+};
