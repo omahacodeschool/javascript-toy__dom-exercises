af1aab018a9fb5a0b20648b0c7b5000f9eec95d3
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-10 16:38:35 -0600

    Uncaught TypeError: Cannot set property 'display' of undefined(anonymous function) @ global.js:15

--- 02-hide-many-elements/global.js
+++ 02-hide-many-elements/global.js
@@ -7,12 +7,12 @@ window.onload = function() {
   click_it.addEventListener("click", function(){
 
   // 3. The block for the listener should get the DOM
-  //    element containing the text to r=hide.
-    var text_to_show = document.getElementByClass("hide_me");
+  //    element containing the text to hide.
+    var text_to_hide = document.getElementsByClassName("hide_me");
 
   // 4. Modify that DOM element's style to change it's 'display'
-  //    from a hidden value to a shown value.
-    text_to_show.style.display = "none";
+  //    from a shown value to a hidden value.
+    text_to_hide.style.display = "none";
 
   });
 
