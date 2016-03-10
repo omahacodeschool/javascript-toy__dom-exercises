28c75da3caab54d8f38747bbc2bd07eba68b3df6
Ryan Newell <youcatastrophe@gmail.com>
2016-03-10 15:03:56 -0600

    Working Code for 05 Toggle Elements

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,19 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+  var toggleButton = document.getElementById("toggle_button");
+
+  toggleButton.addEventListener("click", function() {  
+
+    var toggleDivs = document.querySelectorAll("ul.second_five li");
+
+    for (var i = 0; i < toggleDivs.length; i ++) {
+      if (toggleDivs[i].style.display === "list-item") {
+        toggleDivs[i].style.display = "none"
+      } 
+      else {
+        toggleDivs[i].style.display = "list-item";        
+      }
+    }
+  });
+
+};
