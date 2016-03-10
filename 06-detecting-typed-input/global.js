82a1b410c738475973ba84252c5e1f3232c186ac
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-10 17:00:21 -0600

    YES. ex 6 working beautifully.

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -2,9 +2,6 @@ window.onload = function() {
   var mirror_text = document.getElementById("full_name");
   mirror_text.addEventListener("keyup", function() {
     var letter = document.getElementById("full_name").value;
-    var mirror = document.getElementById("greeting");
-    mirror = letter
-    console.log(letter)
-    console.log(mirror)
+    document.getElementById("greeting").innerHTML = letter
   })//ends function in event listener    
 };//ends window.onload// Your JavaScript goes here.
