3b341b7814877e1cc91c3be82306081bbe4c69d8
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-10 16:34:21 -0600

    finished exercise 02 code work

---
+++ 02-hide-many-elements/global.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+
+  var click_it = document.getElementById("button");
+
+  click_it.addEventListener("click", function(){
+    var textToHide = document.getElementsByClassName("hide_me");
+
+    for (var text = 0, len = textToHide.length; text < len; ++text){
+    textToHide[text].style.visibility = "hidden";
+    }
+  });
+  
+};
