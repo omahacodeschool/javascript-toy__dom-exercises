f6cce2a46870fe9729151e28d94ca28d117138e4
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-10 16:15:18 -0600

    ex 5 done

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here.
+window.onload = function() {  
+  var show_hide_switch = document.getElementById("toggle_button");
+  show_hide_switch.addEventListener("click", function(){
+    var things_to_show = document.querySelectorAll(".second_five li");
+    for (var i = 0; i < things_to_show.length; i++) {
+      if (things_to_show[i].style.display === "none") {
+       things_to_show[i].style.display = "block"; 
+      } else { 
+        things_to_show[i].style.display = "none"     
+      }//ends else   
+    }//ends for loop
+  });//ends show_hide_switch 
+};//ends window.onload
