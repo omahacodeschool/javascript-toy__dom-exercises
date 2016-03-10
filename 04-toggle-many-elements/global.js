58e7157f5959b1c63df2b88eee9b5c5c50ef6367
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-10 17:47:27 -0600

    WIP Exercise 4

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,19 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+
+  var button = getElementById("button");
+
+  button.addEventListener("click", function(){
+
+    var toggle = getElementsByClassName("toggle_me");
+
+    if (toogle.style.display == "none" ){
+      toggle.style.display = "block"; 
+    } else {
+      toggle.style.display == "none";
+    }
+
+  });
+  
+};
+
