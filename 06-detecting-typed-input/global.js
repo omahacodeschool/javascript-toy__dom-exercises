60d3c47e3635ac016f49435eae09f92f8901d1f3
Beth Haubert <bethany.haubert@gmail.com>
2016-03-10 13:38:45 -0600

    06 done

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+	var form = document.getElementById("full_name");
+
+	form.addEventListener("keyup", function() {
+
+		var greeting_para = document.getElementById("greeting");
+
+		greeting_para.innerHTML = ("Hi, " + form.value + "!");
+	});
+
+
+};
