2c9122aa5eceed3aa1f2c0d4caac5bee7fa06be9
Kyle Sandage <ksandage08@gmail.com>
2016-03-10 14:16:49 -0600

    movie toggle works

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,26 @@
 // Your JavaScript goes here.
+window.onload = function() {
+
+  var toggles = document.querySelectorAll("ul.second_five li");
+  
+  var hidden_length = toggles.length;
+
+  click_on_it = document.getElementById("toggle_button");
+
+
+  var hideThings = function(){
+    for (var i = 0; i < hidden_length; i++) {
+      if (toggles[i].style.display == "none") {
+        toggles[i].style.display = "list-item";
+      }
+      else {
+        toggles[i].style.display = "none";
+      };
+    }; 
+  };
+
+  click_on_it.addEventListener("click", hideThings);
+
+
+
+};
