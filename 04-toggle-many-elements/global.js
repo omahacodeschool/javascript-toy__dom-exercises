1036e8deb4f4ca12b70dbd4ec9d8019465e9c459
Amy Wall <amymariewall@gmail.com>
2016-03-10 14:52:00 -0600

    Finished 4, about to head into break before starting 5.

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,27 @@
-// Your JavaScript goes here.
+// Find all of the elements that you want to toggle.
+//When you click a button, those elements should either be shown or hidden.
+// When you click that button again, those elements should be toggled again.
+// You can repeat as many times as you like--the button always "toggles" the display state of the elements.
+
+window.onload = function() {
+
+
+  // 1. Find all elements on the page that you want to toggle.
+
+  var toggledElements = document.getElementsByClassName("toggle_me");
+
+  //When you click a button, those elements should either be shown or hidden.
+  
+  var click_it = document.getElementById("button");
+
+  click_it.addEventListener("click", function(){
+
+
+    for(var i=0; i < toggledElements.length; i++){
+      var el = toggledElements[i];
+      el.style.display = (el.style.display != 'none' ? 'none' : '' );
+
+        }
+    });
+  
+};
