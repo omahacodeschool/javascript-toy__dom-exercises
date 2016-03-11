44b59b3cd5ec9af9c37394a531654ea0d60df4fe
Chris Foral <chris.foral@gmail.com>
2016-03-10 18:16:33 -0600

    Exercise 05 done

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -7,11 +7,11 @@ window.onload = function() {
   var element_to_toggle = document.querySelectorAll("ul.second_five li");
 
     for (var i = 0; i < element_to_toggle.length; i++) {
-      if (element_to_toggle[i].style.display == "block") {
+      if (element_to_toggle[i].style.display == "list-item") {
         element_to_toggle[i].style.display = "none";
       }
       else {
-        element_to_toggle[i].style.display = "block";
+        element_to_toggle[i].style.display = "list-item";
       }
     }
   });
