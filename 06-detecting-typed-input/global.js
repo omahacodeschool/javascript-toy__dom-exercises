a40dbb1b4c3592652fc6248f58e211befabd69dc
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-10 19:13:14 -0600

    DOM Atomic 06 Done

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,4 +1,13 @@
 window.onload = function() {
 
-  
+  var name = document.getElementById("full_name");
+
+  name.addEventListener("keyup", function() {
+
+    var greeting_div = document.getElementById("greeting");
+
+    greeting_div.innerHTML = ("Hello, " + name.value + "!");
+
+  });
+
 };
