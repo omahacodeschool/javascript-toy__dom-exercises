87d93056963e855920b2339687a42920bde25100
Bradley Biehl <bradley.biehl@gmail.com>
2016-03-10 18:53:19 -0600

    DOM Atomic 05 Tasks Done

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -2,8 +2,19 @@ window.onload = function() {
 
   var button = document.getElementById('toggle_button');
 
-  button.addEventListener("click", function(){
-    alert("hi");
-  })
+  button.addEventListener("click", function() {
+
+    var movies = document.querySelectorAll("ul.second_five li");
+
+    for (var i = 0; i < movies.length; i++) {
+      if (movies[i].style.display === "block") {
+        movies[i].style.display = "none";
+      }
+      else {
+        movies[i].style.display = "block";
+      }
+    }
+
+  });
 
 };
