7ee14219f47771582315c3edfe731c1994deda40
Meagan Jungman <meagan.jungman@gmail.com>
2016-03-10 19:53:46 -0600

    finished code for exercise 07

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+  var name = document.getElementById("full_name");
+
+  name.addEventListener("keypress", function() {
+
+  var autoResponse = document.getElementById("greeting");
+
+  autoResponse.textContent = `Hello, ${name.value}`;
+
+});
+
+};
--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,21 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+  var areaCode = document.getElementById('first')
+  areaCodeMaxLength = areaCode.getAttribute("maxlength"),
+  localExchange = document.getElementById('second'),
+  localExchangeMaxLength = localExchange.getAttribute("maxlength"),
+  lineNumber = document.getElementById('third');
+  
+  function fieldShift (element, length, otherElement) {
+    element.addEventListener("keydown", function() {
+        if (element.value.length == length) {
+            otherElement.focus();
+        }
+    });
+  }
+
+
+  fieldShift(areaCode, areaCodeMaxLength, localExchange)
+  fieldShift(localExchange, localExchangeMaxLength, lineNumber)
+
+}
