4ba539cad2559b5429bc3d3eaebccff4a38ed624
Melinda Deinert <melinda.deinert@gmail.com>
2016-03-11 06:55:05 -0600

    Finished

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,23 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+  var paramsOne = document.getElementById("first");
+  var paramsTwo = document.getElementById("second");
+  var paramsThree = document.getElementById("third");
+
+  paramsOne.addEventListener("keyup", function(){
+
+  if (paramsOne.value.length == paramsOne.getAttribute("maxlength")) {
+    paramsTwo.focus()
+  }
+
+});
+
+  paramsTwo.addEventListener("keyup", function(){
+
+  if (paramsTwo.value.length == paramsTwo.getAttribute("maxlength")) {
+    paramsThree.focus()
+  }
+
+});
+
+};
