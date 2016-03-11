3653f2ad68599d04e7dd41c0de2cd8bdf8615df1
Derek Pressnall <derekpressnall@yahoo.com>
2016-03-10 19:42:19 -0600

    Toggle elements

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,25 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+
+  var button = document.getElementById("toggle_button");
+
+  button.addEventListener("click", function(){
+
+    var toggle = document.querySelectorAll(" .movies .second_five ");
+
+    for (var i = 0; i < toggle.length; i++) {
+      if (toggle[i].style.display === "none") {
+        toggle[i].style.display = "block";
+        var children = toggle[i].children;
+        for (var c = 0;c < children.length; c++) {
+          if (children[c].style.display === "none") {
+            children[c].style.display = "block";
+          }
+        }
+      }
+      else { 
+      toggle[i].style.display = "none";
+      }
+    }
+  });
+};
