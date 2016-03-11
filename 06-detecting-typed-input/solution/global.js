d37efc9ae1f17dc5e2c332f0fdb9a0dd89050723
Kyle Sandage <ksandage08@gmail.com>
2016-03-11 08:57:10 -0600

    friday morning push

--- 06-detecting-typed-input/solution/global.js
+++ 06-detecting-typed-input/solution/global.js
@@ -11,3 +11,6 @@ window.onload = function() {
   });
 
 };
+
+
+<img class="alignnone wp-image-2195 size-full" src="//www.bsbdesign.com.s3.amazonaws.com/wp-content/uploads/2015/08/Leader_Dan_Swift.jpg" alt="Dan Swift" width="780" height="300">
