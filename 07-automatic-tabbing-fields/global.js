203d96ce5ef0c60a2a1873bafba01e047b4cfee3
Amy Wall <amymariewall@gmail.com>
2016-03-10 18:44:26 -0600

    Completed Atomic 7.

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,32 @@
-// Your JavaScript goes here.
+// The user sees an HTML form, which has three separate fields for parts of a phone number (ex: 402-555-0000).
+//Write JavaScript so that when a user types in enough characters for each of the first two fields, their cursor automatically moves to the next field.
+
+window.onload = function() {
+
+  
+  var inputFieldOne = document.getElementById("first");
+  var inputFieldTwo = document.getElementById("second");
+  var inputFieldThree = document.getElementById("third");
+
+    inputFieldOne.onkeyup = function(){
+    
+    var maxLength = parseInt(inputFieldOne.attributes["maxlength"].value, 10);
+    var myLength = inputFieldOne.value.length;
+    if (myLength >= maxLength) {
+        inputFieldTwo.focus();
+        }
+    }
+
+ inputFieldTwo.onkeyup = function(){
+    
+    var maxLength = parseInt(inputFieldTwo.attributes["maxlength"].value, 10);
+    var myLength = inputFieldTwo.value.length;
+    if (myLength >= maxLength) {
+        inputFieldThree.focus();
+        }
+    }
+
+
+  
+};
+
