eb1a223f477aaa0782896e6c2dc33c70fc12e865
Chris Foral <chris.foral@gmail.com>
2016-03-11 10:48:24 -0600

    Exerice 07 WIP. Was on right track but had to look at the solution. Working on refactoring

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+  var numOne   = document.getElementById("first")
+  var numTwo   = document.getElementById("second")
+  var numThree = document.getElementById("third")
+
+  numOne.addEventListener("keyup", function() {
+    inputNums = numOne.value.length;
+    maxNums   = parseInt(numOne.getAttribute("maxlength"));
+
+    if (inputNums === maxNums) {
+      numTwo.focus();
+    }
+  });
+
+  numTwo.addEventListener("keyup", function() {
+    inputNums = numTwo.value.length;
+    maxNums   = parseInt(numTwo.getAttribute("maxlength"));
+
+    if (inputNums === maxNums) {
+      numThree.focus();
+    }
+  });
+};
