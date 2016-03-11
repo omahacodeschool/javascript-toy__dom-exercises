2b028cd5aa12c11c47007173e72bfc87206cddfc
Chris Foral <chris.foral@gmail.com>
2016-03-10 18:13:01 -0600

    Exercise 05 WIP. Managed to get the toggle working but the elements are being displayed without their bullets

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+  var click_it = document.getElementById("toggle_button");
+
+  click_it.addEventListener("click", function() {
+
+  var element_to_toggle = document.querySelectorAll("ul.second_five li");
+
+    for (var i = 0; i < element_to_toggle.length; i++) {
+      if (element_to_toggle[i].style.display == "block") {
+        element_to_toggle[i].style.display = "none";
+      }
+      else {
+        element_to_toggle[i].style.display = "block";
+      }
+    }
+  });
+};
