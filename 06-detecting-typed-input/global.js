e51ef97aa26c24a14e79445474a3b00b6589fa9f
Chris Foral <chris.foral@gmail.com>
2016-03-10 19:13:15 -0600

    Exercise 06 done

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,12 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+  var type_it = document.getElementById("full_name");
+
+  type_it.addEventListener("keyup", function() {
+    
+    var greet_it = document.getElementById("greeting");
+    
+    greet_it.innerHTML = ("Hi, " + type_it.value);
+
+  });
+};
