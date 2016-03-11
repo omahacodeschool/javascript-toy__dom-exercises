518af8d6afe941b987b2faf4309c80d3ed092056
Shannon Jackson <shannonfromomaha@gmail.com>
2016-03-10 21:04:44 -0600

    ex 7 WORKS

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,8 +1,19 @@
 window.onload = function() {  
-  var tab_over_first = document.getElementById("first");
-  tab_over_first.addEventListener("keyup", function() {
-    if (tab_over_first.length === 3){
-      //tab over to the next box however you do that
-    }
-  })//ends function in event listener    
+  var first_nums = document.getElementById('first');
+  var second_nums = document.getElementById('second');
+  var third_nums =  document.getElementById('third');
+
+  first_nums.addEventListener("keyup", function() {
+    if (first_nums.value.length === 3){
+      second_nums.focus();
+    }//ends if statement
+  })//ends function in event listener   
+
+  second_nums.addEventListener("keyup", function() {
+    if (second_nums.value.length === 3){
+      third_nums.focus();
+    }//ends if statement
+  })//ends function in event listener  
 };//ends window.onload// Your JavaScript goes here.
+
+
