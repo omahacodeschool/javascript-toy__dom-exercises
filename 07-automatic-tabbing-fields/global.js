0a8ad19c2ea49e3aad1bf50d0a7bd80c6ac7b9ec
Jeremy Stoll <j8r8my@gmail.com>
2016-03-11 11:44:17 -0600

    In Progress. This one is hard.

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,39 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+var numBox1 = document.getElementById("first")
+
+var numBox2 = document.getElementById("second")
+
+var numBox3 = document.getElementById("third")
+
+var inputFields = document.querySelectorAll("input")
+
+var i = 0
+
+debugger;
+
+//Check the value of the text box, and count length. 
+numBox1.addEventListener("keyup", function() {
+  
+  var box1Value = numBox1.value 
+
+  if(box1Value.length == 3){
+    switchInputField()
+    //DO THAT THING WHERE IT TABS.
+    // next("inputs")  ?
+  }
+  else{
+    //DO NOTHING
+  } 
+})
+function switchInputField(){
+  i++
+  inputFields[i]
+
+  
+
+}
+
+
+
+}
