8371a1acf975e47935aaf63161e93222fe66bed7
Jeremy Stoll <j8r8my@gmail.com>
2016-03-11 10:50:29 -0600

    Completed #6

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,11 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+var textField = document.getElementById("full_name")
+var greetingText = document.getElementById("greeting")
+
+textField.addEventListener("keyup", function() {
+
+greetingText.innerHTML = ("Howdy there, " + textField.value + "!");
+
+})
+}
