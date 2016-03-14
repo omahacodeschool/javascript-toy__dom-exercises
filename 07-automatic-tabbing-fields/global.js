4f5eab7b45dc7694a97a785c19f8031ba4a9740e
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-14 13:42:17 -0500

    notes

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,8 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+  //first search document for first id
+  // write code that says
+    // when the length of that entry is == the maxlength
+    // then move the cursor to the next field
+
+};
