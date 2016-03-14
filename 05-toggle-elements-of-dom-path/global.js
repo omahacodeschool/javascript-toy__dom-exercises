9449dc823c63f380855f227b92dcd17f8ea149df
Courtney Burns <courtneylynnburns@gmail.com>
2016-03-14 13:37:50 -0500

    6 is done

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,17 @@
-// Your JavaScript goes here.
+window.onload = function() {
+
+    var toggle_text = function(){
+    texts = document.getElementsByClassName("movies second_five");
+
+    for (var counter = 0; counter < texts.length; counter = counter + 1)
+      if (texts[counter].style.display=="none")
+        texts[counter].style.display = "block";
+      else 
+        texts[counter].style.display = "none";
+    }
+  
+  var hide_button = document.getElementById("toggle_button");
+
+  hide_button.addEventListener("click",toggle_text);
+
+};
--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here.
+
+window.onload = function() {
+
+function display_input(){
+  var get_text = document.getElementById("greeting")
+  get_text.innerHTML = ("Howdy, " + name.value + "!");
+}
+
+var name = document.getElementById("full_name");
+
+name.addEventListener("keypress",display_input);
+
+};
