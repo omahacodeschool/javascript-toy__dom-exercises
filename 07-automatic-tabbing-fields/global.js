6a32ee0194522c9875ec48a337af8ea34a467ca5
Blair Englund <blair.englund@gmail.com>
2016-07-19 17:20:53 -0500

    working on exercise 07

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+	//the text fields that will be affected by an action
+	var areacode = document.getElementById('first');
+	var prefix = document.getElementById('second');
+	var line = document.getElementById('line');
+
+	//create function that will move the cursor if the field is maxxed out
+	var moveCursor = function() {
+
+	};
+
+	//add an Event Listener for each field that will activate on keyup and fire the function to move the cursor
+	areacode.addEventListener("keyup", moveCursor);
+	prefix.addEventListener("keyup", moveCursor);
+	line.addEventListener("keyup", moveCursor);
+
+});
