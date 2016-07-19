cf9389abeda7f8eb3464b085b4a7129ce903c0d4
Blair Englund <blair.englund@gmail.com>
2016-07-19 13:30:11 -0500

    Exercise 03 is functional

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,26 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function() {
+
+	//define the DOM element that will be clicked on
+	var button = document.getElementById('button');
+
+	//define variables for the elements to hide
+	var toggleitems = document.getElementsByClassName('toggle_me');
+
+	//define function that will hide those objects
+
+	var toggle = function() {
+		if (toggleitems[0].style.display == "none") {
+			for (var i = toggleitems.length - 1; i >= 0; i--) {
+				toggleitems[i].style.display = "block";}
+		}
+
+		else if (toggleitems[0].style.display == "block") {
+			for (var i = toggleitems.length - 1; i >= 0; i--) {
+				toggleitems[i].style.display = "none";}
+		}
+	};
+
+	//create an event listener for the button
+	button.addEventListener("click", toggle);
+
+});
